c3b35efaf5f2b9ccd4d07c9c43d0338d4a5d9f40
wjc <wangjianchengcs@163.com>
2024-06-27 15:10:05 +0800

    add wrapper class

--- include/qcu_float_float2_wrapper.h
+++ include/qcu_float_float2_wrapper.h
@@ -18,4 +18,20 @@ struct Float2Wrapper<half> {
     using AccFloat = float;
 };
 
+template <QCU_PRECISION precision>
+struct Float2WrapperFromPrecision {
+    using Float = float;
+    using Float2 = float2;
+};
+template <>
+struct Float2WrapperFromPrecision<QCU_PRECISION::QCU_DOUBLE_PRECISION> {
+    using Float = double;
+    using Float2 = double2;
+};
+template <>
+struct Float2WrapperFromPrecision<QCU_PRECISION::QCU_HALF_PRECISION> {
+    using Float = half;
+    using Float2 = half2;
+};
+
 }  // namespace qcu
