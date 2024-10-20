a6e78055bab8afad73446005af4df501f1c5523c
YellowBicycleee <wangjianchengcs@163.com>
2024-10-20 22:35:15 +0800

    modify: 修改LattDesc，兼容device端

--- include/desc/qcu_desc.h
+++ include/desc/qcu_desc.h
@@ -5,31 +5,49 @@
 
 #include "qcu_public.h"
 #include "qcu.h"
+#include "qcu_helper.h"
 // clang-format off
 
 namespace qcu {
 // in every process, lattice size desc
 struct QcuLattDesc {
     int data[MAX_DIM];
-
-    QcuLattDesc(int x, int y = 1, int z = 1, int t = 1) {
+    const int volume;
+    QCU_HOST_DEVICE
+    QcuLattDesc(int x, int y = 1, int z = 1, int t = 1) : volume (x * y * z * t) {
         data[X_DIM] = x;
         data[Y_DIM] = y;
         data[Z_DIM] = z;
         data[T_DIM] = t;
     }
 
-    QcuLattDesc(QcuParam *param) {
+    QCU_HOST_DEVICE
+    QcuLattDesc(QcuParam *param) :  
+        volume (param->lattice_size[X_DIM] * param->lattice_size[Y_DIM]
+                * param->lattice_size[Z_DIM] * param->lattice_size[T_DIM]
+        )  
+    {
         data[X_DIM] = param->lattice_size[X_DIM];
         data[Y_DIM] = param->lattice_size[Y_DIM];
         data[Z_DIM] = param->lattice_size[Z_DIM];
         data[T_DIM] = param->lattice_size[T_DIM];
     }
+    QCU_HOST_DEVICE
     int latticeDimLength(int dim) const { return data[dim]; }
-    int latticeVolumn() const { return data[X_DIM] * data[Y_DIM] * data[Z_DIM] * data[T_DIM]; }
+
+    QCU_HOST_DEVICE
+    int latticeVolumn() const { return volume; }
+    
+    QCU_HOST_DEVICE
     int X() const { return data[X_DIM]; }
+
+    QCU_HOST_DEVICE
     int Y() const { return data[Y_DIM]; }
+
+    QCU_HOST_DEVICE
     int Z() const { return data[Z_DIM]; }
+
+    QCU_HOST_DEVICE
     int T() const { return data[T_DIM]; }
 };
 
