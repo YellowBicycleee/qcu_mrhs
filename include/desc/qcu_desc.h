407d760f88a4ebecd9151f02e5888383ed547741
YellowBicycleee <wangjianchengcs@163.com>
2024-11-12 17:06:27 +0800

    modify: 将LatticeDesc和ProcessDesc全部调整为CPU + GPU均可以使用

--- include/desc/qcu_desc.h
+++ include/desc/qcu_desc.h
@@ -1,28 +1,23 @@
 #pragma once
 
-#include <cassert>
-#include <cstdio>
-
 #include "qcu_public.h"
 #include "qcu.h"
 #include "qcu_helper.h"
-// clang-format off
 
 namespace qcu {
+
 // in every process, lattice size desc
 struct QcuLattDesc {
     int data[MAX_DIM];
     int volume;
 
-    QCU_HOST_DEVICE
-    QcuLattDesc() : data {0, 0, 0, 0}, volume(0) {}
+    QCU_HOST_DEVICE QcuLattDesc() : data {0, 0, 0, 0}, volume(0) {}
 
-    QCU_HOST_DEVICE
-    QcuLattDesc(int x, int y = 1, int z = 1, int t = 1)
-        : data {x, y, z, t}, volume (x * y * z * t) {}
+    QCU_HOST_DEVICE QcuLattDesc(int x, int y = 1, int z = 1, int t = 1)
+        : data {x, y, z, t}, volume (x * y * z * t)
+    {}
 
-    QCU_HOST_DEVICE
-    QcuLattDesc(QcuParam *param)
+    QCU_HOST_DEVICE QcuLattDesc(QcuParam *param)
         : data {param->lattice_size[X_DIM], param->lattice_size[Y_DIM],
                 param->lattice_size[Z_DIM], param->lattice_size[T_DIM]}
         , volume (param->lattice_size[X_DIM] * param->lattice_size[Y_DIM]
@@ -30,20 +25,13 @@ struct QcuLattDesc {
         )  
     {}
     
-    QCU_HOST_DEVICE
-    int X() const { return data[X_DIM]; }
+    QCU_HOST_DEVICE int X() const { return data[X_DIM]; }
+    QCU_HOST_DEVICE int Y() const { return data[Y_DIM]; }
+    QCU_HOST_DEVICE int Z() const { return data[Z_DIM]; }
+    QCU_HOST_DEVICE int T() const { return data[T_DIM]; }
 
-    QCU_HOST_DEVICE
-    int Y() const { return data[Y_DIM]; }
-
-    QCU_HOST_DEVICE
-    int Z() const { return data[Z_DIM]; }
-
-    QCU_HOST_DEVICE
-    int T() const { return data[T_DIM]; }
-
-    QCU_HOST_DEVICE
-    int lattice_volume() const { return volume; }
+    QCU_HOST_DEVICE int lattice_volume() const { return volume; }
+    QCU_HOST_DEVICE int half_lattice_volume() const { return (volume >> 1); }
 };
 
 struct QcuProcDesc {  // process description, how many process in each dimension
@@ -60,18 +48,18 @@ struct QcuProcDesc {  // process description, how many process in each dimension
             * grid->grid_size[Z_DIM] * grid->grid_size[T_DIM])
     {}
 
-    int X() const { return data[X_DIM]; }
-    int Y() const { return data[Y_DIM]; }
-    int Z() const { return data[Z_DIM]; }
-    int T() const { return data[T_DIM]; }
+    QCU_HOST_DEVICE int X() const { return data[X_DIM]; }
+    QCU_HOST_DEVICE int Y() const { return data[Y_DIM]; }
+    QCU_HOST_DEVICE int Z() const { return data[Z_DIM]; }
+    QCU_HOST_DEVICE int T() const { return data[T_DIM]; }
 
     // x, y, z, t direction is separated
-    bool X_separated() const { return data[X_DIM] > 1; }
-    bool Y_separated() const { return data[Y_DIM] > 1; }
-    bool Z_separated() const { return data[Z_DIM] > 1; }
-    bool T_separated() const { return data[T_DIM] > 1; }
+    QCU_HOST_DEVICE bool X_separated() const { return data[X_DIM] > 1; }
+    QCU_HOST_DEVICE bool Y_separated() const { return data[Y_DIM] > 1; }
+    QCU_HOST_DEVICE bool Z_separated() const { return data[Z_DIM] > 1; }
+    QCU_HOST_DEVICE bool T_separated() const { return data[T_DIM] > 1; }
 
     // volume
-    int process_volume() const { return volume; }
+    QCU_HOST_DEVICE int process_volume() const { return volume; }
 };
 }  // namespace qcu
