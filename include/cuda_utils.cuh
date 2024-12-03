ce69300a3ff2bf905fa2cf476c78d66886247d06
YellowBicycleee <wangjianchengcs@163.com>
2024-12-03 16:05:11 +0800

    modify: 加入了cuda architecture相关的宏，暂时还不正确

--- include/cuda_utils.cuh
+++ include/cuda_utils.cuh
@@ -1,5 +1,11 @@
 #pragma once
 
+#if (!defined(__CUDA_ARCH__) || (__CUDA_ARCH__ >= 800))
+#define QCU_ARCH_WMMA_ENABLED
+#define QCU_ARCH_WMMA_SM80_ENABLED
+#endif
+
+
 __forceinline__ __device__ void cuda_abort() {
     asm("trap;"); // Abort execution and generate an interrupt to the host CPU
 }
