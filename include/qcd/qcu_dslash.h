62b4a23430c0344bedc4d838136402fc6246aaaf
YellowBicycleee <wangjianchengcs@163.com>
2024-12-25 21:46:34 +0800

    modify: 加入了ghost部分

--- include/qcd/qcu_dslash.h
+++ include/qcd/qcu_dslash.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "qcu_config/qcu_config.h"
 #include "desc/qcu_desc.h"
 #include "qcu_public.h"
+#include "data_format/fermion.cuh"
 #include <cuda_runtime.h>
 #include <memory>
 namespace qcu {
@@ -14,15 +16,15 @@ struct DslashParam {
     int m_input;
     int parity;
     double kappa;
-
     void* __restrict__ fermion_in_MRHS;
     void* __restrict__ fermion_out_MRHS;
     void* __restrict__ gauge;
     const QcuLattDesc* __restrict__ latt_desc;
     const QcuProcDesc* __restrict__ proc_desc;
+
     cudaStream_t stream1;
     cudaStream_t stream2;
-
+    std::shared_ptr<qcu::FermionGhost<Nd>> fermion_ghost;
     DslashParam(
         bool dagger_flag_,
         QcuPrecision dslash_precision_,
@@ -36,21 +38,25 @@ struct DslashParam {
         const QcuLattDesc* latt_desc_,
         const QcuProcDesc* proc_desc_,
         cudaStream_t stream1_ = nullptr,
-        cudaStream_t stream2_ = nullptr)
+        cudaStream_t stream2_ = nullptr,
+        std::shared_ptr<qcu::FermionGhost<Nd>> fermion_ghost_ = nullptr
+        )
 
-        : dagger_flag(dagger_flag_),
-        dslash_precision(dslash_precision_),
-        n_color(n_color_),
-        m_input(m_input_),
-        parity(parity_),
-        kappa(kappa_),
-        fermion_in_MRHS(fermion_in_MRHS_),
-        fermion_out_MRHS(fermion_out_MRHS_),
-        gauge(gauge_),
-        proc_desc(proc_desc_),
-        latt_desc(latt_desc_),
-        stream1(stream1_),
-        stream2(stream2_) {}
+    : dagger_flag(dagger_flag_)
+    , dslash_precision(dslash_precision_)
+    , n_color(n_color_)
+    , m_input(m_input_)
+    , parity(parity_)
+    , kappa(kappa_)
+    , fermion_ghost(fermion_ghost_)
+    , fermion_in_MRHS(fermion_in_MRHS_)
+    , fermion_out_MRHS(fermion_out_MRHS_)
+    , gauge(gauge_)
+    , proc_desc(proc_desc_)
+    , latt_desc(latt_desc_)
+    , stream1(stream1_)
+    , stream2(stream2_)
+    {}
 };
 
 class Dslash {
@@ -59,7 +65,7 @@ public:
     virtual ~Dslash() noexcept = default;
     virtual void apply(const std::shared_ptr<DslashParam> dslash_param) = 0;
     virtual double flops() = 0;
-
+    virtual double operations() const { return operations_cur_; }
 protected:
     inline static double operations_total_ = 0.0;
     inline static double time_utilization_total_ = 0.0;
