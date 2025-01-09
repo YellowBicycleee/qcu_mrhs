2b4daffe3a52963eeace18e9b646805d4754596f
YellowBicycleee <wangjianchengcs@163.com>
2025-01-09 22:41:07 +0800

    modify: 修改ld fermion的实现，经测试，yzt单个轴通过

--- include/kernel/sun_mrhs_wilson_dslash_unpack_simt.cuh
+++ include/kernel/sun_mrhs_wilson_dslash_unpack_simt.cuh
@@ -125,10 +125,14 @@ void single_point_wilson_dslash_t_forward_ghost_unpack(
                         scale = kernel::Gamma<FloatType_>::get_projection_scale(ghost_dim, mat1_pos, dir);
                         if (dagger_flag) { scale = -scale; }
                     }
-                    gemm::ldg_fermion<FloatType_, FermionMatShape, BlockShape_, WarpShape_> (
-                        reinterpret_cast<FloatType_*>(glb_B + mat1_pos * fermion_site_length),
-                        reinterpret_cast<FloatType_*>(glb_B + mat2_pos * fermion_site_length),
-                        n_color, m_rhs, scale, k, col, reinterpret_cast<Float2_t<FloatType_> *>(ldg_B));
+                    gemm::ldg<Float2, FermionMatShape, BlockShape_, WarpShape_> (
+                        reinterpret_cast<Float2*>(glb_B) + pos * n_color * m_rhs,
+                        n_color, m_rhs, row, col,
+                        reinterpret_cast<Float2*>(ldg_B));
+                    // gemm::ldg_fermion<FloatType_, FermionMatShape, BlockShape_, WarpShape_> (
+                    //     reinterpret_cast<FloatType_*>(glb_B + mat1_pos * fermion_site_length),
+                    //     reinterpret_cast<FloatType_*>(glb_B + mat2_pos * fermion_site_length),
+                    //     n_color, m_rhs, scale, k, col, reinterpret_cast<Float2_t<FloatType_> *>(ldg_B));
                     gemm::sts_direct<Float2, FermionMatShape, BlockShape_, WarpShape_>
                             (&smem_B[0][pos * B_Shape], reinterpret_cast<Float2*>(ldg_B));
                 }
