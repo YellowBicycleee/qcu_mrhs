90808b2ef7d10e50b84558377f713ec89d4ca2d3
YellowBicycleee <wangjianchengcs@163.com>
2025-03-17 16:17:48 +0800

    modify:加入simt和tensorop控制

---
+++ include/qcu_cuda_config.cuh
@@ -0,0 +1,16 @@
+#pragma once
+
+
+namespace qcu::config {
+
+// use these classes to initialize the template parameters
+namespace cudaOp {
+struct TensorOp {};
+struct Simt {};
+}
+
+namespace tensorCoreConfig {
+
+}
+
+}
