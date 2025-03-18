0224aadba7eb8f04f198a07b36910d6f8369e641
YellowBicycleee <wangjianchengcs@163.com>
2025-03-18 15:57:51 +0800

    modify：norm和dot均改为cublas接口

--- src/qcu_blas/qcu_complex_dotc.cu
+++ src/qcu_blas/qcu_complex_dotc.cu
@@ -22,26 +22,54 @@ using DotcArgument = typename qcu::qcu_blas::ComplexDotc<OutputFloat, InputFloat
 // ReductionInnerprod
 template <typename OutputFloat, typename InputFloat>
 void ComplexDotc<OutputFloat, InputFloat>::operator()(DotcArgument arg) {
+    cublasHandle_t cublas_handle = arg.handle;
+    if (nullptr == cublas_handle) {
+        throw std::runtime_error("cublas handle is nullptr");
+    }
+    if constexpr (std::is_same_v<InputFloat, double>) {
+        int stride = arg.stride;
+        for (int i = 0; i < stride; ++i) {
+            QCU_CHECK_CUBLAS (
+                cublasZdotc(cublas_handle, arg.single_vec_len,
+                    reinterpret_cast<const cuDoubleComplex*>(arg.input1) + i, stride,
+                    reinterpret_cast<const cuDoubleComplex*>(arg.input2) + i, stride,
+                    reinterpret_cast<cuDoubleComplex*>(arg.resArr) + i
+                ));
+        }
+    }
+    else if constexpr (std::is_same_v<InputFloat, float>) {
+        int stride = arg.stride;
+        for (int i = 0; i < stride; ++i) {
+            QCU_CHECK_CUBLAS (
+                cublasCdotc(cublas_handle, arg.single_vec_len,
+                    reinterpret_cast<const cuComplex*>(arg.input1) + i, stride,
+                    reinterpret_cast<const cuComplex*>(arg.input2) + i, stride,
+                    reinterpret_cast<cuComplex*>(arg.resArr) + i
+                )
+            );
+        }
+    }
+    else {
+        int threads_per_block            = std::min(512, maxThreadsPerBlock);
+        int blocks_per_grid              = std::min(div_ceil(arg.single_vec_len, threads_per_block), maxGridSize);
+        int thread_round1                = threads_per_block;
+        int block_round1                 = blocks_per_grid;
+        // 1 个 warp打底，不然会出现 warp divergence
+        int thread_round2 = std::min( div_ceil(block_round1, kWarpSize) * kWarpSize, maxThreadsPerBlock);
+        int block_round2  = 1;
 
-    int threads_per_block            = std::min(512, maxThreadsPerBlock);
-    int blocks_per_grid              = std::min(div_ceil(arg.single_vec_len, threads_per_block), maxGridSize);
-    int thread_round1                = threads_per_block;
-    int block_round1                 = blocks_per_grid;
-    // 1 个 warp打底，不然会出现 warp divergence
-    int thread_round2 = std::min( div_ceil(block_round1, kWarpSize) * kWarpSize, maxThreadsPerBlock);
-    int block_round2  = 1;
-
-    for (int i = 0; i < arg.stride; ++i) {
-        device::reduction::stride_ComplexInnerProd_step1_kernel <qcu::device::operation::AddOp, OutputFloat, InputFloat>
-            <<<block_round1, thread_round1>>>
-                (reinterpret_cast<OutputFloat*> (arg.tmpBuffer), reinterpret_cast<InputFloat*>  (arg.input1),
-                reinterpret_cast<InputFloat*>  (arg.input2), i, arg.stride, arg.single_vec_len);
-        CHECK_CUDA(cudaGetLastError());
-        // second step
-        device::reduction::reduceSumStep2_kernel <qcu::device::operation::AddOp, Complex<OutputFloat>, qcu::device::operation::UnaryOp> // norm2 开根号得出norm
-            <<<block_round2, thread_round2>>>
-                (arg.resArr, arg.tmpBuffer, i, blocks_per_grid);
-        CHECK_CUDA(cudaGetLastError());
+        for (int i = 0; i < arg.stride; ++i) {
+            device::reduction::stride_ComplexInnerProd_step1_kernel <qcu::device::operation::AddOp, OutputFloat, InputFloat>
+                <<<block_round1, thread_round1>>>
+                    (reinterpret_cast<OutputFloat*> (arg.tmpBuffer), reinterpret_cast<InputFloat*>  (arg.input1),
+                    reinterpret_cast<InputFloat*>  (arg.input2), i, arg.stride, arg.single_vec_len);
+            CHECK_CUDA(cudaGetLastError());
+            // second step
+            device::reduction::reduceSumStep2_kernel <qcu::device::operation::AddOp, Complex<OutputFloat>, qcu::device::operation::UnaryOp> // norm2 开根号得出norm
+                <<<block_round2, thread_round2>>>
+                    (arg.resArr, arg.tmpBuffer, i, blocks_per_grid);
+            CHECK_CUDA(cudaGetLastError());
+        }
     }
     CHECK_CUDA(cudaStreamSynchronize(arg.stream));
 
@@ -65,33 +93,33 @@ void ComplexDotc<OutputFloat, InputFloat>::operator()(DotcArgument arg) {
     }
 }
 
-template<>
-void ComplexDotc<double, double>::operator() (DotcArgument arg) {
-    cublasHandle_t cublas_handle = arg.handle;
-    if (nullptr == cublas_handle) {
-        throw std::runtime_error("cublas handle is nullptr");
-    }
-    int stride = arg.stride;
-    for (int i = 0; i < stride; ++i) {
-        QCU_CHECK_CUBLAS (
-            cublasZdotc(cublas_handle, arg.single_vec_len,
-                reinterpret_cast<const cuDoubleComplex*>(arg.input1) + i, stride,
-                reinterpret_cast<const cuDoubleComplex*>(arg.input2) + i, stride,
-                reinterpret_cast<cuDoubleComplex*>(arg.resArr) + i
-            ));
-    }
+// template<>
+// void ComplexDotc<double, double>::operator() (DotcArgument arg) {
+//     cublasHandle_t cublas_handle = arg.handle;
+//     if (nullptr == cublas_handle) {
+//         throw std::runtime_error("cublas handle is nullptr");
+//     }
+//     int stride = arg.stride;
+//     for (int i = 0; i < stride; ++i) {
+//         QCU_CHECK_CUBLAS (
+//             cublasZdotc(cublas_handle, arg.single_vec_len,
+//                 reinterpret_cast<const cuDoubleComplex*>(arg.input1) + i, stride,
+//                 reinterpret_cast<const cuDoubleComplex*>(arg.input2) + i, stride,
+//                 reinterpret_cast<cuDoubleComplex*>(arg.resArr) + i
+//             ));
+//     }
 
-    auto mpi_separated_mask = qcu::config::get_mpi_separated_mask();
-    if (mpi_separated_mask > 0) {
-        double local_norm;
-        double global_norm;
-        CHECK_CUDA(cudaMemcpy(&local_norm, arg.resArr, sizeof(double) * 2, cudaMemcpyDeviceToHost));
-        local_norm = local_norm * local_norm;
-        CHECK_MPI(MPI_Allreduce(&local_norm, &global_norm, 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD));
-        global_norm = std::sqrt(global_norm);
-        CHECK_CUDA(cudaMemcpy(arg.resArr, &global_norm, sizeof(double) * 2, cudaMemcpyHostToDevice));
-    }
-}
+//     auto mpi_separated_mask = qcu::config::get_mpi_separated_mask();
+//     if (mpi_separated_mask > 0) {
+//         double local_norm;
+//         double global_norm;
+//         CHECK_CUDA(cudaMemcpy(&local_norm, arg.resArr, sizeof(double) * 2, cudaMemcpyDeviceToHost));
+//         local_norm = local_norm * local_norm;
+//         CHECK_MPI(MPI_Allreduce(&local_norm, &global_norm, 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD));
+//         global_norm = std::sqrt(global_norm);
+//         CHECK_CUDA(cudaMemcpy(arg.resArr, &global_norm, sizeof(double) * 2, cudaMemcpyHostToDevice));
+//     }
+// }
 template struct ComplexDotc<double, double>;
 template struct ComplexDotc<float, float>;
 template struct ComplexDotc<double, half>;
--- src/qcu_blas/qcu_complex_norm.cu
+++ src/qcu_blas/qcu_complex_norm.cu
@@ -23,32 +23,52 @@ using ComplexNormArgument = typename qcu::qcu_blas::ComplexNorm<OutputFloat, Inp
 // Norm
 template <typename OutputFloat, typename InputFloat>
 void ComplexNorm<OutputFloat, InputFloat>::operator()(ComplexNormArgument param) {
-    int threads_per_block            = std::min(512, maxThreadsPerBlock);
-    int blocks_per_grid              = std::min(div_ceil(param.single_vec_len, threads_per_block), maxGridSize);
+    if constexpr(std::is_same_v<InputFloat, float>) {
+        QCU_CHECK_CUBLAS (
+            cublasScnrm2(param.handle,
+                        param.single_vec_len,
+                        reinterpret_cast<cuComplex*>(param.input), 
+                        param.stride,
+                        reinterpret_cast<float*>(param.resArr))
+        );
+    }
+    else if constexpr(std::is_same_v<InputFloat, float>) {
+        QCU_CHECK_CUBLAS (
+            cublasDznrm2(param.handle, 
+                        param.single_vec_length, 
+                        reinterpret_cast<cuDoubleComplex*>(param.input), 
+                        param.stride, 
+                        reinterpret_cast<double*>(param.resArr))
+        );
+    }
+    else {
+        int threads_per_block            = std::min(512, maxThreadsPerBlock);
+        int blocks_per_grid              = std::min(div_ceil(param.single_vec_len, threads_per_block), maxGridSize);
 
-    int thread_round1                = threads_per_block;
-    int block_round1                 = blocks_per_grid;
+        int thread_round1                = threads_per_block;
+        int block_round1                 = blocks_per_grid;
 
-    // 1 个 warp打底，不然会出现规约错误
-    int thread_round2                = std::min(div_ceil(block_round1, kWarpSize) * kWarpSize, maxThreadsPerBlock);
-    int block_round2                 = 1;
+        // 1 个 warp打底，不然会出现规约错误
+        int thread_round2                = std::min(div_ceil(block_round1, kWarpSize) * kWarpSize, maxThreadsPerBlock);
+        int block_round2                 = 1;
 
-
-    for (int i = 0; i < param.stride; ++i) {
-        // first step
-        device::reduction::stride_ComplexNorm_step1_kernel <qcu::device::operation::AddOp, OutputFloat, InputFloat>
-            <<<block_round1, thread_round1, 0, param.stream>>>
-                (reinterpret_cast<OutputFloat*> (param.tmpBuffer), reinterpret_cast<InputFloat*>  (param.input),
-                    i, param.stride, param.single_vec_len);
-        CHECK_CUDA(cudaGetLastError());
-        // second step
-        device::reduction::reduceSumStep2_kernel <qcu::device::operation::AddOp, OutputFloat, qcu::device::operation::SqrtOp>
-            <<<block_round2, thread_round2, 0, param.stream>>>
-                (reinterpret_cast<OutputFloat*>(param.resArr), reinterpret_cast<OutputFloat*>(param.tmpBuffer),
-                    i, blocks_per_grid);
-        CHECK_CUDA(cudaGetLastError());
+        for (int i = 0; i < param.stride; ++i) {
+            // first step
+            device::reduction::stride_ComplexNorm_step1_kernel <qcu::device::operation::AddOp, OutputFloat, InputFloat>
+                <<<block_round1, thread_round1, 0, param.stream>>>
+                    (reinterpret_cast<OutputFloat*> (param.tmpBuffer), reinterpret_cast<InputFloat*>  (param.input),
+                        i, param.stride, param.single_vec_len);
+            CHECK_CUDA(cudaGetLastError());
+            // second step
+            device::reduction::reduceSumStep2_kernel <qcu::device::operation::AddOp, OutputFloat, qcu::device::operation::SqrtOp>
+                <<<block_round2, thread_round2, 0, param.stream>>>
+                    (reinterpret_cast<OutputFloat*>(param.resArr), reinterpret_cast<OutputFloat*>(param.tmpBuffer),
+                        i, blocks_per_grid);
+            CHECK_CUDA(cudaGetLastError());
+        }
     }
     CHECK_CUDA(cudaStreamSynchronize(param.stream));
+    
     MPI_Datatype mpi_datatype;
     if constexpr (std::is_same_v<OutputFloat, double>) {
         mpi_datatype = MPI_DOUBLE;
@@ -65,12 +85,12 @@ void ComplexNorm<OutputFloat, InputFloat>::operator()(ComplexNormArgument param)
 
         CHECK_CUDA(cudaMemcpy(local_norm, param.resArr, sizeof(OutputFloat) * num_rhs, cudaMemcpyDeviceToHost));
         
-#pragma omp parallel for
+        #pragma omp parallel for
         for (int i = 0; i < num_rhs; ++i) {
             local_norm[i] = local_norm[i] * local_norm[i];
         }
         CHECK_MPI(MPI_Allreduce(local_norm, global_norm, num_rhs, mpi_datatype, MPI_SUM, MPI_COMM_WORLD));
-#pragma omp parallel for
+        #pragma omp parallel for
         for (int i = 0; i < num_rhs; ++i) {
             global_norm[i] = std::sqrt(global_norm[i]);
         }
